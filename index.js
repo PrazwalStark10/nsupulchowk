74bb98d1a189bf1760f80ab8d075aa480412ad1f
PrazwalStark10 <prazwalstark@gmail.com>
2023-03-16 17:24:51 +0545

    website ready 70%

---
+++ index.js
@@ -0,0 +1,81 @@
+// for modal of feedback and donation
+const fb = document.querySelector(".nav-btn.feedback");
+const dnt = document.querySelector('.nav-btn.donate');
+const fbclose = document.querySelector('.feedbackclose');
+const confb = document.querySelector('.con-feedback');
+const dntclose = document.querySelector('.donationclose');
+const dntfb = document.querySelector('.con-donation');
+fb.addEventListener('click',function(){
+    console.log("feedback button pressed");
+    console.log(confb.classList);
+    if(!confb.classList.contains('show-con-feedback')){
+        confb.classList.add('show-con-feedback');
+        console.log(confb.classList);
+    }
+});
+fbclose.addEventListener('click',function(){
+    if(confb.classList.contains('show-con-feedback')){
+        confb.classList.remove('show-con-feedback');
+    }
+});
+// .container-feedback
+
+
+dnt.addEventListener('click',function(){
+    console.log("Donation button pressed");
+    console.log(dntfb.classList);
+    if(!dntfb.classList.contains('show-con-donation')){
+        dntfb.classList.add('show-con-donation');
+        console.log(dntfb.classList);
+    }
+});
+dntclose.addEventListener('click',function(){
+    if(dntfb.classList.contains('show-con-donation')){
+        dntfb.classList.remove('show-con-donation');
+    }
+});
+//.container- for slider
+// Get the DOM elements for the image carousel
+const wrapper = document.querySelector(".wrapper"),
+  carousel = document.querySelector(".carousel"),
+  images = document.querySelectorAll("img"),
+  buttons = document.querySelectorAll(".button");
+
+let imageIndex = 1,
+  intervalId;
+
+// Define function to start automatic image slider
+const autoSlide = () => {
+  // Start the slideshow by calling slideImage() every 2 seconds
+  intervalId = setInterval(() => slideImage(++imageIndex), 2000);
+};
+// Call autoSlide function on page load
+autoSlide();
+
+// A function that updates the carousel display to show the specified image
+const slideImage = () => {
+  // Calculate the updated image index
+  imageIndex = imageIndex === images.length ? 0 : imageIndex < 0 ? images.length - 1 : imageIndex;
+  // Update the carousel display to show the specified image
+  carousel.style.transform = `translate(-${imageIndex * 100}%)`;
+};
+
+// A function that updates the carousel display to show the next or previous image
+const updateClick = (e) => {
+  // Stop the automatic slideshow
+  clearInterval(intervalId);
+  // Calculate the updated image index based on the button clicked
+  imageIndex += e.target.id === "next" ? 1 : -1;
+  slideImage(imageIndex);
+  // Restart the automatic slideshow
+  autoSlide();
+};
+
+// Add event listeners to the navigation buttons
+buttons.forEach((button) => button.addEventListener("click", updateClick));
+
+// Add mouseover event listener to wrapper element to stop auto sliding
+wrapper.addEventListener("mouseover", () => clearInterval(intervalId));
+// Add mouseleave event listener to wrapper element to start auto sliding again
+wrapper.addEventListener("mouseleave", autoSlide);
+
